505a056f720dbd685e23a7650e2d8a23f90e8e4e
iceland101113 <r02521115@ntu.edu.tw>
2018-07-24 13:03:24 +0800

    complete get 10% off discount for buy 3 different episode book testing

---
+++ lib/cart.rb
@@ -0,0 +1,33 @@
+class Cart
+
+  PRICE = 100     # 每本書的單價是 100 元
+
+  def add(items)
+    @ordered_items = items.map{ |key,value| value }.delete_if{ |i| i == 0 }   # 把資料由 Hash 轉成陣列
+  end
+
+  def calculate    # 計算客戶所買的書本總價
+    total = 0
+
+      while @ordered_items.size > 0
+        
+        if @ordered_items.size == 2
+          
+          total = total + 2 * PRICE * 0.95
+
+        elsif @ordered_items.size == 3
+
+          total = total + 3 * PRICE * 0.9
+                            
+        else
+          # 把書拿出來計算
+          total = total + PRICE
+        end
+        @ordered_items = @ordered_items.map{ |i| i -= 1 }
+        @ordered_items = @ordered_items.delete_if{ |i| i == 0 }
+      end
+
+    return total
+  end
+
+end
---
+++ spec/cart_spec.rb
@@ -0,0 +1,76 @@
+require_relative '../lib/cart.rb'
+
+describe Cart do
+
+  before do
+    @cart = Cart.new
+  end
+
+  context "第一種情境：不打折" do
+
+    it "第一集買 1 本" do
+      @cart.add({ "1st": 1, "2nd": 0, "3rd": 0, "4th": 0, "5th": 0 })
+      expect(@cart.calculate).to eq(100)
+    end
+
+    it "第一集買 3 本" do
+      @cart.add({ "1st": 3, "2nd": 0, "3rd": 0, "4th": 0, "5th": 0 })
+      expect(@cart.calculate).to eq(300)
+    end
+
+  end
+
+  context "第二種情境：打 5% 折扣" do
+
+    it "第一集買 1 本、第二集買 1 本" do
+      @cart.add({ "1st": 1, "2nd": 1, "3rd": 0, "4th": 0, "5th": 0 })
+      expect(@cart.calculate).to eq(190)
+    end
+
+    it "第一集買 2 本、第二集買 2 本" do
+      @cart.add({ "1st": 2, "2nd": 2, "3rd": 0, "4th": 0, "5th": 0 })
+      expect(@cart.calculate).to eq(380)
+    end
+
+    it "第一集買 1 本、第二集買 2 本" do
+      @cart.add({ "1st": 1, "2nd": 2, "3rd": 0, "4th": 0, "5th": 0 })
+      expect(@cart.calculate).to eq(290)
+    end
+
+    it "第一集買 2 本、第二集買 3 本" do
+      @cart.add({ "1st": 2, "2nd": 3, "3rd": 0, "4th": 0, "5th": 0 })
+      expect(@cart.calculate).to eq(480)
+    end
+
+  end
+
+  context "第三種情境：打 10% 折扣" do
+
+    it "第一集買 1 本、第二集買 1 本、第三集買 1 本" do
+      @cart.add({ "1st": 1, "2nd": 1, "3rd": 1, "4th": 0, "5th": 0 })
+      expect(@cart.calculate).to eq(270)
+    end
+
+    it "第三集買 1 本、第四集買 1 本、第五集買 1 本" do
+      @cart.add({ "1st": 0, "2nd": 0, "3rd": 1, "4th": 1, "5th": 1 })
+      expect(@cart.calculate).to eq(270)
+    end  
+
+    it "第三集買 2 本、第四集買 2 本、第五集買 2 本" do
+      @cart.add({ "1st": 0, "2nd": 0, "3rd": 2, "4th": 2, "5th": 2 })
+      expect(@cart.calculate).to eq(540)
+    end  
+
+    it "第三集買 2 本、第四集買 3 本、第五集買 2 本" do
+      @cart.add({ "1st": 0, "2nd": 0, "3rd": 2, "4th": 3, "5th": 2 })
+      expect(@cart.calculate).to eq(640)
+    end   
+
+    it "第三集買 2 本、第四集買 3 本、第五集買 4 本" do
+      @cart.add({ "1st": 0, "2nd": 0, "3rd": 2, "4th": 3, "5th": 4 })
+      expect(@cart.calculate).to eq(830)
+    end     
+
+  end
+
+end
